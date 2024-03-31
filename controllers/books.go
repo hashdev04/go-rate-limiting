935251f379cf86353c8c757057ce32445cf8918a
hashdev04 <hashimjj6t@gmail.com>
2024-03-31 14:33:03 +0400

    add other rest routes

--- controllers/books.go
+++ controllers/books.go
@@ -12,10 +12,15 @@ type CreateBookInput struct {
 	Author string `json:"author" binding:"required"`
 }
 
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 func FindBooks(c *gin.Context) {
 	var books []models.Book
-
 	models.DB.Find(&books)
+
 	c.JSON(http.StatusOK, gin.H{"data": books})
 }
 
@@ -32,4 +37,51 @@ func CreateBook(c *gin.Context) {
 	models.DB.Create(&book)
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func FindBook(c *gin.Context) {
+	var book models.Book
+	err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+	err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	var input UpdateBookInput
+	err = c.ShouldBindJSON(&input)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+	err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,7 @@ import (
 func Routes(router *gin.Engine) {
 	router.GET("/books", controllers.FindBooks)
 	router.POST("/books", controllers.CreateBook)
+	router.GET("/books/:id", controllers.FindBook)
+	router.PATCH("/books/:id", controllers.UpdateBook)
+	router.DELETE("/books/:id", controllers.DeleteBook)
 }
